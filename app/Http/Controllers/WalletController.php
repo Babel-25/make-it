42dfe0a17f79f5ca1588d9afb883151541c0bd1f
philIII <philippesf3@gmail.com>
2023-02-24 15:19:18 +0000

    Affichage montant sur page portefeuille

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Montant;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
     public function walletForm()
     {
-        return view('layout.user.wallets.portefeuille');
-    }
+        $user = User::find(auth()->user()->id)->personne;
+        $montant_user = Montant::where('personne_id', $user->id)->first();
+        $montant_parrainage =  $montant_user->gain_parrainage;
+        $montant_phase1 =  $montant_user->gain_niv1 +  $montant_user->gain_niv2 +
+            $montant_user->gain_niv3 +  $montant_user->gain_niv4;
 
+        $montant_net = $montant_parrainage + $montant_phase1;
+        return view('layout.user.wallets.portefeuille', compact('montant_user', 'montant_parrainage', 'montant_phase1','montant_net'));
+    }
 }
